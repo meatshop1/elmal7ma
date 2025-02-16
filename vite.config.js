ca0a6f18c7dc28c06dae6ed6405346a6413dc642
BRHM1 <brhm@std.mans.edu.eg>
2025-02-16 04:52:49 +0200

    Added build.rollupOptions.external

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "./runtimeConfig": "./runtimeConfig.browser",
+  build: {
+    rollupOptions: {
+      external: ['react-icons/fa'], // Explicitly mark react-icons/fa as external
     },
   },
 })
